037fb9bfde8746202f31ede35a0ff8d10c41b307
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-09-09 11:56:19 -0700

    Add files via upload

---
+++ ArrayPractice.java
@@ -0,0 +1,31 @@
+// aj ArrayPractice.java 9/9/25
+
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+
+
+        System.out.println("\n WElcome to my Reverse String Warm up program");
+
+
+        // create a String array of four elements
+        String[] myFourStrings = new String[4];
+        // Fill the array
+        myFourStrings[0] = "one";
+        myFourStrings[1] = "two";
+        myFourStrings[2] = "three";
+        myFourStrings[3] = "four";
+
+        // Output the array
+        for (int i=0; i<4;i++) {
+            System.out.println("\n i is " + i + " and myFourStrings[" + i + "] is: " + myFourStrings[i]);
+        }
+
+
+
+
+
+
+
+    }
+}
