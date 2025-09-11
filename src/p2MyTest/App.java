42313cc98ff9bf179384ad504a2033e560808a22
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-09-11 11:48:14 -0700

    Add files via upload

---
+++ src/p2MyTest/App.java
@@ -0,0 +1,14 @@
+package p2MyTest;
+
+public class App {
+    public static void main(String[] args) {
+
+
+        Games Vidya = new Games();
+        Vidya.typeOfGame = "The Finals";
+
+        System.out.println("My favorite game to play is " + Vidya.typeOfGame);
+
+
+    }
+}
